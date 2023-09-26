18232116049efb7293ca8e8b85cef49b7e2d2c3d
John Guerrero <je.guerreroa1@uniandes.edu.co>
2023-09-25 23:38:31 -0500

    feat: Add fonts and graphical definitions to the application

--- mobile/app/src/main/java/edu/co/uniandes/optitask/ui/home/HomeFragment.kt
+++ mobile/app/src/main/java/edu/co/uniandes/optitask/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -17,26 +18,50 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var hours = 0
+    private var minutes = 0
+    private var seconds = 0
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // Set click listeners for numbered buttons
+        for (i in 0..9) {
+            val buttonId = resources.getIdentifier("button_$i", "id", requireActivity().packageName)
+            val button = root.findViewById<Button>(buttonId)
+            button.setOnClickListener {
+                // Handle button click, update hours/minutes/seconds
+                when {
+                    seconds < 60 -> seconds = seconds * 10 + i
+                    minutes < 60 -> {
+                        seconds = i
+                        minutes = minutes * 10 + i
+                    }
+                    else -> {
+                        seconds = i
+                        minutes = i
+                        hours = hours * 10 + i
+                    }
+                }
+                updateTimerDisplay()
+            }
         }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateTimerDisplay() {
+        val time = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        binding.timerDisplay.text = time
     }
 }
